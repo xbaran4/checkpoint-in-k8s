09e5c95f7d4dcc7792f2e86a9f1146f9821cbf8d
Pavol Baran <492953@muni.cz>
2024-12-13 12:17:25 +0100

    added documentation for storage.go

--- checkpointer/pkg/manager/storage.go
+++ checkpointer/pkg/manager/storage.go
@@ -8,19 +8,39 @@ import (
 	"github.com/peterbourgon/diskv/v3"
 )
 
+// CheckpointEntry represent the result of a container checkpointing request.
 type CheckpointEntry struct {
+	// ContainerIdentifier represents the container that was checkpointed.
 	ContainerIdentifier checkpoint.ContainerIdentifier `json:"containerIdentifier"`
-	BeginTimestamp      int64                          `json:"beginTimestamp"`
-	EndTimestamp        int64                          `json:"endTimestamp"`
-	ContainerImageName  string                         `json:"containerImageName"`
-	Error               error                          `json:"error,omitempty"`
+
+	// BeginTimestamp is a Unix timestamp representing the time checkpointing was initiated.
+	BeginTimestamp int64 `json:"beginTimestamp"`
+
+	// EndTimestamp is a Unix timestamp representing the time checkpointing was finished.
+	EndTimestamp int64 `json:"endTimestamp"`
+
+	// ContainerImageName represents the container image that is pushed to a remote container registry.
+	ContainerImageName string `json:"containerImageName"`
+
+	// Error is the error that might have occurred during checkpointing.
+	Error error `json:"error,omitempty"`
 }
 
+// CheckpointStorage is responsible for storing CheckpointEntry instances.
 type CheckpointStorage interface {
+	// StoreEntry stores CheckpointEntry under the given checkpointIdentifier key.
+	// Returns error on fail or nil otherwise.
 	StoreEntry(checkpointIdentifier string, entry CheckpointEntry) error
+
+	// ReadEntry reads CheckpointEntry stored under checkpointIdentifier key.
+	// Returns error on fail. If there is CheckpointEntry stored under given key, returns pointer to a CheckpointEntry
+	// instance, otherwise returns nil pointer.
 	ReadEntry(checkpointIdentifier string) (*CheckpointEntry, error)
 }
 
+// checkpointDiskStorage stores instances of CheckpointEntry as files on the file system using storageBackend.
+// As storageBackend backend only support storage of bytes, the CheckpointEntry instances are unmarshalled/marshalled
+// for read/write.
 type checkpointDiskStorage struct {
 	storageBackend *diskv.Diskv
 }
@@ -38,7 +58,10 @@ func (cs *checkpointDiskStorage) StoreEntry(checkpointIdentifier string, entry C
 	if err != nil {
 		return fmt.Errorf("failed to marshal checkpoint entry: %w", err)
 	}
-	return cs.storageBackend.Write(checkpointIdentifier, marshalled)
+	if err := cs.storageBackend.Write(checkpointIdentifier, marshalled); err != nil {
+		return fmt.Errorf("failed to write checkpoint entry: %w", err)
+	}
+	return nil
 }
 
 func (cs *checkpointDiskStorage) ReadEntry(checkpointIdentifier string) (*CheckpointEntry, error) {
